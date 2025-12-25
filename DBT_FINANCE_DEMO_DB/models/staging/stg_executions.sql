c47ecbaed7a0046494c8e6ce887d96813d98b386
hkawai <h.kawai@flux-g.com>
2025-12-25 13:28:32 +0900

    OK

---
+++ DBT_FINANCE_DEMO_DB/models/staging/stg_executions.sql
@@ -0,0 +1,20 @@
+-- models/staging/stg_executions.sql
+
+with source as (
+    select * from {{ source('finance_raw', 'executions') }}
+),
+
+renamed as (
+    select
+        execution_id,
+        account_id,
+        symbol as stock_symbol,
+        side as trade_side,
+        quantity as trade_quantity,
+        price as trade_price,
+        execution_at as executed_at,
+        commission
+    from source
+)
+
+select * from renamed
